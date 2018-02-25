9d339c26f41bddc1004c20b21bde739e45e74f97
Sapendo <slavagorban1983@gmail.com>
2018-02-25 17:49:39 +0200

    homework 10

--- homework_09/js/cypherPhrase.js
+++ homework_09/js/cypherPhrase.js
@@ -1,7 +1,6 @@
 let cypherPhrase = (obj, str) => {
 	let arrOfKeys = Object.keys(obj),
-		arrFromStr = str.split(''),
+		arrFromStr = [...str],
 		newArray = [];
 	newArray = getTransformedArray(arrFromStr, el => arrOfKeys.indexOf(el) !== -1 ? obj[el] : el);
 	return newArray.join('');
-}
---
+++ homework_10/task1.js
@@ -0,0 +1,31 @@
+function debounce(fn, time) {
+	let clearId;
+  return function () {
+    clearTimeout(clearId);
+    clearId = setTimeout(fn, time);
+}
+}
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+let increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 1'
